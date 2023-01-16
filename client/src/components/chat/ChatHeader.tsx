d3e684a0194107ec34674adcacd01511038bf1ce
Delvoid <delvoid.dev@gmail.com>
2023-01-16 00:54:14 +0000

    add room name

--- client/src/components/chat/ChatHeader.tsx
+++ client/src/components/chat/ChatHeader.tsx
@@ -1,9 +1,12 @@
 import Button from '../Button';
+import { Socket } from 'socket.io-client';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 const ChatHeader = () => {
+  const [room, setRoom] = useLocalStorage<string>('room', '');
   return (
     <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
-      <div className="font-semibold text-2xl">Room Name</div>
+      <div className="font-semibold text-2xl">{room}</div>
 
       <div className="">
         <Button type="submit" variant="primary" label="Leave Chat" />
